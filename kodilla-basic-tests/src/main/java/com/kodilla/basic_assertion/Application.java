96b28bc7f112f2ac0e1651d8247668c07ac4b650
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-03-30 19:11:16 +0200

    3.7v2

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -1,6 +1,7 @@
-package com.kodilla.basic_asseration;
+package com.kodilla.basic_assertion;
 
 public class Application {
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         int a = 5;
