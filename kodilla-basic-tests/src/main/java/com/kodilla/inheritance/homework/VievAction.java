f7470a7d86fd51ce9eba8db37355fc92f91cf535
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-03-30 21:11:42 +0200

    3.3v3

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/VievAction.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/VievAction.java
@@ -3,19 +3,18 @@ package com.kodilla.inheritance.homework;
 public class VievAction {
     public static void main(String[] args) {
 
-        OperatingSystem win95 = new OperatingSystem(95);
-        OperatingSystem win98 = new OperatingSystem(98);
-        OperatingSystem win2000 = new OperatingSystem(2000);
+        Windows windows95 = new Windows(1995);
+        OperatingSystem operatingSystem = new OperatingSystem(1980);
+        Linux linux = new Linux(2000);
 
-        //OperatingSystem.turnOn();
-        //Linux.turnOn(); // Metoda nadpisana.
+        operatingSystem.turnOff();
+        windows95.turnOff(); //metoda nadpisana
+        operatingSystem.turnOn();
+        linux.turnOn(); // Metoda nadpisana
 
-        System.out.println(win95.getYear());
-        System.out.println(win95.getYear());
-
-       // OperatingSystem.turnOff();
-       // Windows.turnOff(); // Metoda nadpisana.
-        win2000.displaySystemYear();
+        operatingSystem.displaySystemYear();
+        windows95.displaySystemYear();
+        linux.displaySystemYear();
 
     }
 }
