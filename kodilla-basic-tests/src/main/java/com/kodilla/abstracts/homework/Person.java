5bc5d74082dab96e515188731ebbd91f9e60b709
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-04-02 16:55:01 +0200

    3.4v5

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -3,34 +3,28 @@ package com.kodilla.abstracts.homework;
 public class Person {
 
     String firstName;
-    String job;
+    Job job;
     int age;
 
-    public Person(String firstName, int age, String job, int salary, String responsibilities){
+    public Person(String firstName, int age, Job job){
        this.firstName = firstName;
        this.age = age;
        this.job = job;
    }
 
     public void displayInformation() {
-        System.out.print("His name is " + firstName + ", he is " + age + " years old and works as " + job + ".");
+        System.out.println("His name is " + firstName + ", he is " + age + " years old and his duty is " + job.responsibilities + ".");
     }
 
     public static void main(String[] args) {
 
-        Policeman policeman1 = new Policeman(1100, "reasponsibilities1");
-        Reporter reporter1 = new Reporter(1200,"responsibilities2");
-        TaxiDriver taxiDriver1 = new TaxiDriver(1000, "responsibilities3");
-        Person policeman = new Person("Adam",34,"policeman",1100, "responsibilities1");
-        Person reporter = new Person("Jan",52,"reporter", 1150,"responsibilities2");
-        Person taxiDriver = new Person("Krzysztof",41,"taxidriver",998,"responsibilities3");
+        Person policeman = new Person("Adam",34,new Policeman(1100,"responsibilities1"));
+        Person reporter = new Person("Jan",52,new Reporter(1200,"responsibilities2"));
+        Person taxiDriver = new Person("Krzysztof",41,new TaxiDriver(990,"responsibilities3"));
 
         policeman.displayInformation();
-        policeman1.displayResponsibilities();
         reporter.displayInformation();
-        reporter1.displayResponsibilities();
         taxiDriver.displayInformation();
-        taxiDriver1.displayResponsibilities();
 
     }
 }
