1e81a0f510ce6574626dfd6c4189a8481fa81ae5
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-04-21 21:38:13 +0200

    6.4v3

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -2,20 +2,27 @@ package com.kodilla.bank.homework;
 
 public class Bank {
 
+
+        CashMachine cashMachine1 = new CashMachine(new int[]{150, 50, -200, -50});
+        CashMachine cashMachine2 = new CashMachine(new int[]{250, 150, -250, -200});
+        CashMachine cashMachine3 = new CashMachine(new int[]{100, 250, -150, -100});
+        CashMachine cashMachine4 = new CashMachine(new int[]{200, 200, -100, -250});
+        CashMachine cashMachine5 = new CashMachine(new int[]{100, 100, -50, -250});
+
+        CashMachine[] cashMachines = new CashMachine[] {cashMachine1,cashMachine2, cashMachine3, cashMachine4, cashMachine5};
+
+    public int getTotalBalance() {
+        int sum = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            for(int e = 0; e < cashMachines[e].length; e ++) //tu mam problem
+                sum = sum + cashMachine[e]; //i tu mam problem
+            }
+        return sum;
+    }
+
     public static void main(String[] args) {
-
-        CashMachine cashMachine1 = new CashMachine();
-        CashMachine cashMachine2 = new CashMachine();
-        CashMachine cashMachine3 = new CashMachine();
-        CashMachine cashMachine4 = new CashMachine();
-        CashMachine cashMachine5 = new CashMachine();
-
-        CashMachine[] cashMachines = new CashMachine[5];
-        cashMachines[0] = cashMachine1;
-        cashMachines[1] = cashMachine2;
-        cashMachines[2] = cashMachine3;
-        cashMachines[3] = cashMachine4;
-        cashMachines[4] = cashMachine5;
-
+        Bank bank = new Bank();
+       // System.out.println(bank.getTotalBalance());
+        System.out.println(bank.cashMachines[3]);
     }
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -1,8 +1,18 @@
 package com.kodilla.bank.homework;
 
+import java.util.Arrays;
+
 public class CashMachine {
 
-    int [] cashMachine = new int [] {100, -200, 200, -50};
+    private int [] cashMachine;
+
+    public CashMachine(int[] cashMachine) {
+        this.cashMachine = cashMachine;
+    }
+
+    public int[] getCashMachine() {
+        return cashMachine;
+    }
 
     public int getBalance() {
         int sum = 0;
@@ -12,6 +22,13 @@ public class CashMachine {
         return sum;
     }
 
+    @Override
+    public String toString() {
+        return "CashMachine{" +
+                 Arrays.toString(cashMachine) +
+                '}';
+    }
+
     public int getNumberOfTransactions () {
         int numberOfElements = cashMachine.length;
         return numberOfElements;
@@ -19,7 +36,7 @@ public class CashMachine {
 
     public static void main(String[] args) {
 
-        CashMachine machine = new CashMachine();
+        CashMachine machine = new CashMachine(new int[] {50,150,-250,55});
         System.out.println("Liczba transakcji wynosi " + machine.getNumberOfTransactions());
         System.out.println("Saldo bankomatu wynosi " + machine.getBalance());
 
