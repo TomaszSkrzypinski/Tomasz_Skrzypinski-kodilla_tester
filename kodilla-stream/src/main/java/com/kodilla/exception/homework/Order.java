8b3a33acde8d99706483834e1916d7846f8f8467
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-04-21 12:47:07 +0200

    6.4v2

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Order.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Order.java
@@ -1,11 +1,12 @@
 package com.kodilla.exception.homework;
 
 public class Order {
-    String number;
+    private String number;
 
     public Order(String number) {
         this.number = number;
     }
+    
     public String getNumber() {
         return number;
     }
--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class Warehouse {
 
-    List <Order> orders = new LinkedList <>();
+    List<Order> orders = new LinkedList<>();
 
-    public List <Order> addOrder(Order order) {
+    public List<Order> addOrder(Order order) {
         orders.add(order);
         return null;
     }
 
     public Order getOrder(String number) throws OrderDoesntExistException {
-        orders
-                .stream()
+        orders.stream()
                 .filter(u -> u.getNumber().equals(number))
                 .forEach(u -> System.out.println("number " + u));
         throw new OrderDoesntExistException();
+        return ; //powinienem zwrócić jakis Order, ale jak bym nie kombinował podkreśla mi na czerwono; potrzebuję podpowiedź :)
     }
 }
 
