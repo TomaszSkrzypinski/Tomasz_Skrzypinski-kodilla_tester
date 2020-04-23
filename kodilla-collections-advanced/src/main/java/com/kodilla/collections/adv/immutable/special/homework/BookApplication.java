52f9eb04e5c537714536d08be16b639efd13a97c
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-04-23 14:11:19 +0200

    5.5v3

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -5,11 +5,14 @@ public class BookApplication {
 
        BookManager bookManager = new BookManager();
 
-        Book book1 = bookManager.createBook("title1", "author1");
-        Book book2 = bookManager.createBook("title2", "author2");
-        Book book3 = bookManager.createBook("title3", "author3");
+       Book book1 = bookManager.createBook("title1", "author1");
+       Book book2 = bookManager.createBook("title2", "author2");
+       Book book3 = bookManager.createBook("title3", "author3");
+       Book book4 = bookManager.createBook("title1", "author1");
 
-        System.out.println(book1 == book2);
-        System.out.println(book3 == book2);
-    }
+       System.out.println(book1 == book2);
+       System.out.println(book3 == book2);
+       System.out.println(book1 == book4);
+       System.out.println(book4);
+   }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -3,28 +3,28 @@ package com.kodilla.collections.adv.immutable.special.homework;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookManager extends BookApplication{
-    
-    public Book createBook(String title, String author) {
+public class BookManager extends BookApplication {
 
-        return new Book(title, author);
+    List <Book> books = new ArrayList <>();
+
+    public void addBook(Book book) {
+        books.add(book);
+        for (Book book1 : books)
+            System.out.println(book1.getAuthor() + " " + book1.getTitle());
     }
 
-    public static void main(String[] args) {
-
-        BookManager bookManager = new BookManager();
-
-        List <Book> books = new ArrayList <>();
-        Book book1 = bookManager.createBook("title1", "author1");
-        Book book2 = bookManager.createBook("title2", "author2");
-        Book book3 = bookManager.createBook("title3", "author3");
-        books.add(book1); //Jak zrobić, żeby przypisać do kolekcji obiekt "book1" bezpośrednio z klasy BookApplication?
-        books.add(book2);
-        books.add(book3);
-
-        for (Book book : books)
-            System.out.println(book);
-
+    public Book createBook(String title, String author) {
+        if (books.size() == 0) {
+            addBook(new Book(title, author));
+            return new Book(title, author);
+        } else
+            for (int i = 0; i < books.size(); i++) {
+                if (title.equals(books.get(i).getTitle()) && author.equals(books.get(i).getAuthor())) {
+                    return books.get(i);}
+                addBook(new Book(title, author));
+                return new Book(title, author);
+            }
+        return null;
     }
 }
 
