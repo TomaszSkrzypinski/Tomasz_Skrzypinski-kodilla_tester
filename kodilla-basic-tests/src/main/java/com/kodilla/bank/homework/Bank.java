924a539d8df0aa3fe922f97bd5abc3a44903a0d2
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-04-24 13:25:28 +0200

    3.10v6

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -2,11 +2,11 @@ package com.kodilla.bank.homework;
 
 public class Bank {
 
-        CashMachine cashMachine1 = new CashMachine(new int[]{150, -100, -150, -200});
-        CashMachine cashMachine2 = new CashMachine(new int[]{100, 100, -100, -200});
-        CashMachine cashMachine3 = new CashMachine(new int[]{150, 250, -150, -100});
-        CashMachine cashMachine4 = new CashMachine(new int[]{-150, 200, -150, -250});
-        CashMachine cashMachine5 = new CashMachine(new int[]{100, 100, -100, -250});
+        CashMachine cashMachine1 = new CashMachine(new int[]{100, 100, 100, 100});
+        CashMachine cashMachine2 = new CashMachine(new int[]{-100, -100, -100, -100});
+        CashMachine cashMachine3 = new CashMachine(new int[]{});
+        CashMachine cashMachine4 = new CashMachine(new int[]{100});
+        CashMachine cashMachine5 = new CashMachine(new int[]{-100});
 
         CashMachine[] cashMachines = new CashMachine[] {cashMachine1,cashMachine2, cashMachine3, cashMachine4, cashMachine5};
 
@@ -18,30 +18,30 @@ public class Bank {
         return sum;
     }
 
-    public double getAverageOfAllPositiveTransactions() {
-        double avg = getBalanceOfAllPositiveTransactions()/getNumberOfAllPositiveTransactions();
-        return avg;
-    }
-
-    public double getAverageOfAllNegativeTransactions() {
-        double avg = getBalanceOfAllNegativeTransactions()/getNumberOfAllNegativeTransactions();
-        return avg;
-    }
-
-    public int getBalanceOfAllPositiveTransactions() {
-        int sum = 0;
+    public int getNumberOfAllNegativeTransactions() {
+        int number = 0;
         for (int i = 0; i < cashMachines.length; i++) {
-            sum = sum + cashMachines[i].getBalanceOfPositiveTransactions();
+            number += cashMachines[i].getNumberOfNegativeTransactions();
         }
-        return sum;
+        return number;
     }
 
-    public int getBalanceOfAllNegativeTransactions() {
-        int sum = 0;
-        for (int i = 0; i < cashMachines.length; i++) {
-            sum = sum + cashMachines[i].getBalanceOfNegativeTransactions();
-        }
-        return sum;
+    public int zeroNegativeTransactions() {
+        int number = 0;
+            number += cashMachines[0].getNumberOfNegativeTransactions();
+        return number;
+    }
+
+    public int zeroNegativeTransactions2() {
+        int number = 0;
+            number += cashMachines[2].getNumberOfNegativeTransactions();
+        return number;
+    }
+
+    public int oneNegativeTransactions() {
+        int number = 0;
+        number += cashMachines[4].getNumberOfNegativeTransactions();
+        return number;
     }
 
     public int getNumberOfAllPositiveTransactions() {
@@ -52,12 +52,68 @@ public class Bank {
         return number;
     }
 
-    public int getNumberOfAllNegativeTransactions() {
+    public int zeroPositiveTransactions() {
         int number = 0;
-        for (int i = 0; i < cashMachines.length; i++) {
-            number += cashMachines[i].getNumberOfNegativeTransactions();
-        }
+        number += cashMachines[1].getNumberOfPositiveTransactions();
         return number;
+    }
+
+    public int zeroPositiveTransactions2() {
+        int number = 0;
+        number += cashMachines[2].getNumberOfPositiveTransactions();
+        return number;
+    }
+
+    public int onePositiveTransactions() {
+        int number = 0;
+        number += cashMachines[3].getNumberOfPositiveTransactions();
+        return number;
+    }
+
+    public double getAverageOfAllNegativeTransactions() {
+        double avg = getBalanceOfAllNegativeTransactions()/getNumberOfAllNegativeTransactions();
+        return avg;
+    }
+
+    public double averageOfZeroNegativeTransactions() {
+        double avg = zeroNegativeTransactions()/zeroNegativeTransactions();
+        return avg;
+    }
+
+    public double averageOfZeroNegativeTransaction2() {
+        double avg = zeroNegativeTransactions2()/zeroNegativeTransactions2();
+        return avg;
+    }
+
+    public double getAverageOfAllPositiveTransactions() {
+        double avg = getBalanceOfAllPositiveTransactions()/getNumberOfAllPositiveTransactions();
+        return avg;
+    }
+
+    public double averageOfZeroPositiveTransactions() {
+        double avg = zeroPositiveTransactions()/zeroPositiveTransactions();
+        return avg;
+    }
+
+    public double averageOfZeroPositiveTransaction2() {
+        double avg = zeroPositiveTransactions2()/zeroPositiveTransactions2();
+        return avg;
+    }
+
+    public int getBalanceOfAllNegativeTransactions() {
+        int sum = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            sum = sum + cashMachines[i].getBalanceOfNegativeTransactions();
+        }
+        return sum;
+    }
+
+    public int getBalanceOfAllPositiveTransactions() {
+        int sum = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            sum = sum + cashMachines[i].getBalanceOfPositiveTransactions();
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuit.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuit.java
@@ -10,48 +10,118 @@ public class BankTestSuit {
     public void totalBalance() {
          Bank bank = new Bank();
          int result = bank.getTotalBalance();
-         assertEquals(-750, result);
-    }
-
-    @Test
-    public void negativeTransactions() {
-        Bank bank = new Bank();
-        int result = bank.getBalanceOfAllNegativeTransactions();
-        assertEquals(-1900,result);
-    }
-
-    @Test
-    public void positiveTransactions() {
-        Bank bank = new Bank();
-        int result = bank.getBalanceOfAllPositiveTransactions();
-        assertEquals(1150,result);
-    }
-
-    @Test
-    public void averageNegativeTransactions() {
-        Bank bank = new Bank();
-        double result = bank.getAverageOfAllNegativeTransactions();
-        assertEquals(-158.0, result);
-    }
-
-    @Test
-    public void averagePositiveTransactions() {
-        Bank bank = new Bank();
-        double result = bank.getAverageOfAllPositiveTransactions();
-        assertEquals(143.0, result);
-    }
-
-    @Test
-    public void numberOfAllPositiveTransactions() {
-        Bank bank = new Bank();
-        int result = bank.getNumberOfAllPositiveTransactions();
-        assertEquals(8, result);
+         assertEquals(0, result);
     }
 
     @Test
     public void numberOfAllNegativeTransactions() {
         Bank bank = new Bank();
         int result = bank.getNumberOfAllNegativeTransactions();
-        assertEquals(12, result);
+        assertEquals(5, result);
     }
+
+    @Test
+    public void zeroNegativeTransactions() {
+        Bank bank = new Bank();
+        int result = bank.zeroNegativeTransactions();
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void zeroNegativeTransactions2() {
+        Bank bank = new Bank();
+        int result = bank.zeroNegativeTransactions2();
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void oneNegativeTransactions() {
+        Bank bank = new Bank();
+        int result = bank.oneNegativeTransactions();
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void numberOfAllPositiveTransactions() {
+        Bank bank = new Bank();
+        int result = bank.getNumberOfAllPositiveTransactions();
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void zeroPositiveTransactions() {
+        Bank bank = new Bank();
+        int result = bank.zeroPositiveTransactions();
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void zeroPositiveTransactions2() {
+        Bank bank = new Bank();
+        int result = bank.zeroPositiveTransactions2();
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void onePositiveTransactions() {
+        Bank bank = new Bank();
+        int result = bank.onePositiveTransactions();
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void averageNegativeTransactions() {
+        Bank bank = new Bank();
+        double result = bank.getAverageOfAllNegativeTransactions();
+        assertEquals(-100.0, result);
+    }
+
+    @Test
+    public void averageOfZeroNegativeTransactions() {
+        Bank bank = new Bank();
+        double result = bank.zeroNegativeTransactions();
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void averageOfZeroNegativeTransactions2() {
+        Bank bank = new Bank();
+        double result = bank.zeroNegativeTransactions2();
+        assertEquals(0.0, result);
+    }
+
+    @Test
+    public void averagePositiveTransactions() {
+        Bank bank = new Bank();
+        double result = bank.getAverageOfAllPositiveTransactions();
+        assertEquals(100.0, result);
+    }
+
+    @Test
+    public void averageOfZeroPositiveTransactions() {
+        Bank bank = new Bank();
+        double result = bank.zeroPositiveTransactions();
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void averageOfZeroPositiveTransactions2() {
+        Bank bank = new Bank();
+        double result = bank.zeroPositiveTransactions2();
+        assertEquals(0.0, result);
+    }
+
+//    @Test
+//    public void negativeTransactions() {
+//        Bank bank = new Bank();
+//        int result = bank.getBalanceOfAllNegativeTransactions();
+//        assertEquals(-500,result);
+//    }
+//
+//    @Test
+//    public void positiveTransactions() {
+//        Bank bank = new Bank();
+//        int result = bank.getBalanceOfAllPositiveTransactions();
+//        assertEquals(500,result);
+//    }
 }
