09e2a48b1fcd4e0298ad2a994d150d73361a7813
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-05-30 17:01:18 +0200

    13.4v2

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,11 +1,12 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
 
 public class AllegroTestingApp {
     public static void main(String[] args) throws InterruptedException {
@@ -13,13 +14,16 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl");
 
-        WebElement yearCombo = driver.findElement(
-                By.xpath("/html/body/div[2]/div[3]/header/div/div/div/div/form/div[2]/div/select"));
+        TimeUnit.SECONDS.sleep(5);
 
+        WebElement dialogWindow = driver.findElement(By.xpath("//button[@data-role='accept-consent']"));
+        dialogWindow.click();
+
+        WebElement yearCombo = driver.findElement(By.xpath("//select[@data-role='filters-dropdown-toggle']"));
         Select yearSelect = new Select(yearCombo);
         yearSelect.selectByIndex(3);
 
-        WebElement inputField = driver.findElement(By.xpath("/html/body/div[2]/div[3]/header/div/div/div/div/form/input"));
+        WebElement inputField = driver.findElement(By.xpath("//input[@role='combobox']"));
         inputField.sendKeys("Mavic mini");
         inputField.submit();
     }
