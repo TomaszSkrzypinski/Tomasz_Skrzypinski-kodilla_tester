a8e98425a5394f34fbeef0f57d6e462986d449e2
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-06-21 14:55:01 +0200

    15.4v1

---
+++ kodilla-code-coverage/src/main/java/com/kodilla/hibernate/invoice/repository/InvoiceRepository.java
@@ -0,0 +1,10 @@
+package com.kodilla.hibernate.invoice.repository;
+
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface InvoiceRepository {
+}
---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/invoice/repository/InvoiceRepositoryTestSuite.java
@@ -0,0 +1,11 @@
+package com.kodilla.hibernate.invoice.repository;
+
+import org.junit.Test;
+
+public class InvoiceRepositoryTestSuite {
+
+    @Test
+    public void testInvoiceRepositorySave() {
+
+    }
+}
