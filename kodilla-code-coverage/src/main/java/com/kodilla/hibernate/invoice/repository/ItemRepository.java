ac786c5bfee99be6e8fcfa885ff9798b76f7756f
Tomasz Skrzypinski <4a_skrzypinski@wp.pl>
2020-06-22 19:27:40 +0200

    15.4v2

--- kodilla-code-coverage/src/main/java/com/kodilla/hibernate/invoice/repository/ItemRepository.java
+++ kodilla-code-coverage/src/main/java/com/kodilla/hibernate/invoice/repository/ItemRepository.java
@@ -1,10 +1,12 @@
 package com.kodilla.hibernate.invoice.repository;
 
+import com.kodilla.hibernate.invoice.Item;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Transactional
 @Repository
-public interface ItemRepository {
+public interface ItemRepository extends CrudRepository <Item, Integer> {
 }
